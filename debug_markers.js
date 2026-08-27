72d800a16f638b4bdda8c9ba69dae05d276b3428
Liam Farishta <liamfarishta@Liams-Air.local>
2026-08-27 10:16:44 +0530

    Merge all JS files into main.js and cleanup workspace

--- debug_markers.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let bm = fs.readFileSync('bmwalker.js', 'utf8');
-
-let match = bm.match(/calculateEatPosture\([\s\S]*?return eatMarkers;/);
-if (match) {
-    console.log(match[0]);
-}
